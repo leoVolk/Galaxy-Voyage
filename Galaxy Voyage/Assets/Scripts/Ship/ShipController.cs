f30974607334a2b5631835452b137114bed8fb25
Leo Volkmann <leovolkmann@googlemail.com>
2020-05-18 23:11:55 +0200

    misc

--- Galaxy Voyage/Assets/Scripts/Ship/ShipController.cs
+++ Galaxy Voyage/Assets/Scripts/Ship/ShipController.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-
-    private CharacterController controller;
     private float throttle;
     private float strafe;
     private float rotation;
     private Vector3 mousePos;
 
+
+
     void Awake()
     {
-        controller = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -32,24 +31,34 @@ public class ShipController : MonoBehaviour
 
     void UpdatePositionWithKeyboardInput()
     {
-        //transform.position += transform.forward * throttle * Time.deltaTime;
+        transform.position += transform.forward * throttle * Time.deltaTime;
         //transform.position += transform.right * strafe * Time.deltaTime;
-        controller.Move(transform.forward * throttle * Time.deltaTime);
     }
 
 
-    void UpdateRotationWithKeyBoardInput(){
+    void UpdateRotationWithKeyBoardInput()
+    {
         transform.RotateAround(transform.position, transform.up, rotation * Time.deltaTime);
     }
 
     public void LookAtMousePos()
     {
         transform.forward = Vector3.Lerp(transform.forward, mousePos - transform.position, Time.deltaTime * rotation);
-        
+
     }
 
-    public void SetMousePosition(Vector3 mousePos){
+    public void SetMousePosition(Vector3 mousePos)
+    {
         this.mousePos = mousePos;
+    }
+
+
+    //TODO: find good way to check collision and thereby disable movement
+    // or just leave it at that and let the play run the ship against walls while taking massive damage
+    bool CheckCollision()
+    {
+        
+        return false;
     }
 
 }
